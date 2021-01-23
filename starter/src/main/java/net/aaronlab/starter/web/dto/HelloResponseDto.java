041f5f4d29fbae33e15b5abef18eb8e0222d59c9
Aaron Lee <aaronlab.net@gmail.com>
2021-01-23 20:32:00 +0900

    init hello controller

---
+++ starter/src/main/java/net/aaronlab/starter/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package net.aaronlab.starter.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
