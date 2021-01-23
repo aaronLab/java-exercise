f0c24241aa538e39850bfb00f799597ff4d7470c
Aaron Lee <aaronlab.net@gmail.com>
2021-01-23 19:38:23 +0900

    init starter

---
+++ starter/src/test/java/net/aaronlab/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aaronlab.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
