fb1a33d17e8d3c0a4de48a2ea2554c54bcd2ab8c
Aaron Lee <aaronlab.net@gmail.com>
2021-01-28 01:36:12 +0900

    server init

---
+++ dddtdd/src/test/java/com/example/dddtdd/DddtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddtddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
